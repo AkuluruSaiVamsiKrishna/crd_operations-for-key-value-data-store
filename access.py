7c79ca23ea272aced4f642b80eab7829b314d09f
AkuluruSaiVamsiKrishna <45092521+AkuluruSaiVamsiKrishna@users.noreply.github.com>
2020-12-29 09:19:34 +0530

    Update access.py

--- access.py
+++ access.py
@@ -2,7 +2,7 @@ import code as x
 #importing the main file
 
 
-x.create("sastra",25)
+x.create("vamsi",25)
 #to create a key with key_name,value given and with no time-to-live property
 
 
@@ -10,7 +10,7 @@ x.create("src",70,3600)
 #to create a key with key_name,value given and with time-to-live property value given(number of seconds)
 
 
-x.read("sastra")
+x.read("vamsi")
 #it returns the value of the respective key in Jasonobject format 'key_name:value'
 
 
@@ -18,18 +18,18 @@ x.read("src")
 #it returns the value of the respective key in Jasonobject format if the TIME-TO-LIVE IS NOT EXPIRED else it returns an ERROR
 
 
-x.create("sastra",50)
+x.create("vamsi",50)
 #it returns an ERROR since the key_name already exists in the database
 #To overcome this error 
 #either use modify operation to change the value of a key
 #or use delete operation and recreate it
 
 
-x.modify("sastra",55)
+x.modify("vamsi",55)
 #it replaces the initial value of the respective key with new value 
 
  
-x.delete("sastra")
+x.delete("vamsi")
 #it deletes the respective key and its value from the database(memory is also freed)
 
 #we can access these using multiple threads like
